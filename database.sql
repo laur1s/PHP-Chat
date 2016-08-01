43804c7afe1b4d65c727aaa4a61553f62415916d
Laurynas <l.tumosa@lancaster.ac.uk>
2016-08-01 16:50:07 +0300

    fixed database

--- database.sql
+++ database.sql
@@ -23,30 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shouts`
+-- Table structure for table `messages`
 --
 
-CREATE TABLE IF NOT EXISTS `shouts` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `message` text NOT NULL,
   `time` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `shouts`
 --
 
-INSERT INTO `shouts` (`id`, `user`, `message`, `time`) VALUES
-(1, 'Paul', 'Quisque dignissim a lorem quis venenatis. Curabitur ultricies', '11:20:00'),
-(2, 'Brad', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis', '11:23:00'),
-(3, 'John', 'Pellentesque vitae mauris non odio elementum feugiat at et orci.', '11:24:00'),
-(4, 'Sam', 'Vestibulum a dapibus sem. Proin magna justo, imperdiet egestas cursus in, interdum sit amet ipsum. Vivamus facilisis, quam non placerat congue,', '11:30:00'),
-(5, 'Jen', 'Maecenas rhoncus dapibus eros, in consequat odio blandit in. Quisque dignissim a lorem quis venenatis. Curabitur ultricies', '11:50:00'),
-(6, 'Brad', 'This is a new message', '12:50:42'),
-(7, 'Brad', 'This is a new message', '12:51:09'),
-(8, 'John', 'An even newer message', '12:51:27');
+INSERT INTO `messages` (`id`, `user`, `message`, `time`) VALUES
+(1, 'John', 'Mauris sodales sem at elementum egestas.', '11:20:00'),
+(2, 'Tom', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', '02:02:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
